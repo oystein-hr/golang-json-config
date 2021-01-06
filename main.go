78490d689009d72b8c8c41401468baf6463e2599
oystein-hr <oystein.github@hofvind-rom.com>
2021-01-06 15:07:27 +0100

    Grunnkode på plass.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+// Config is ...
+type Config struct {
+	Token string `json:"token"`
+	Tall  int    `json:"tall"`
+}
+
+var jsonFile string = "./.config"
+var config Config = loadConfiguration(jsonFile)
 
 func main() {
-	log.Println("Temp")
+	fmt.Println("Token string:", config.Token)
+	fmt.Println("Tall int:", config.Tall)
+}
+
+func loadConfiguration(file string) Config {
+	var config Config
+
+	configFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Println(err)
+	}
+
+	json.Unmarshal(configFile, &config)
+
+	log.Println(file, "is loaded and ready.")
+	return config
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+# JSON fil for golang settings
+Enkelt eksempel på å bruke en JSON fil for settings som skal brukes i Go. Kan brukes for eksempel til tokens (discord bot), http settings (port osv.), mm.
